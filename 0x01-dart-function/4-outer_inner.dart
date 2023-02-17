6021e1728ec4cf677fdc343e26e476db6e6a9518
Tim Simms <tsimms@gmail.com>
2023-02-17 15:20:09 -0600

    remove extra newline

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -2,7 +2,7 @@ void outer(String name, String id)
 {
     String inner() {
     var names = name.split(" ");
-    return "Hello Agent ${names[1][0]}.${names[0]} your id is $id\n";
+    return "Hello Agent ${names[1][0]}.${names[0]} your id is $id";
   }
   print(inner());
 }
