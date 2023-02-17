fe8a78748ef2a5e65ee32a50f680bd5f0f48cdb6
Tim Simms <tsimms@gmail.com>
2023-02-17 13:45:45 -0600

    init task

---
+++ 0x00-hello-dart/0-hello_holberton.dart
@@ -0,0 +1,9 @@
+/// this a documentation comment --  used to document the purpose, behavior, 
+// this is a normal one line comment
+/*
+this is the prefered
+multiline comment
+*/ 
+void main() {
+	print("Hello World");
+}
---
+++ 0x00-hello-dart/README.md
@@ -0,0 +1,4 @@
+0x00. Dart - Hello Dart
+
+
+[Task Link](https://intranet.hbtn.io/projects/2831)
