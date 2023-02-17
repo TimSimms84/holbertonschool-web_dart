e4b69702142699e7d80e659f97637f2f778cec84
Tim Simms <tsimms@gmail.com>
2023-02-17 13:49:31 -0600

    task 1

---
+++ 0x00-hello-dart/1-quotes.dart
@@ -0,0 +1,9 @@
+/// this a documentation comment --  used to document the purpose, behavior, 
+// this is a normal one line comment
+/*
+this is the prefered
+multiline comment
+*/ 
+void main() {
+	print('"Programming is like building a multilingual puzzle\n');
+}
