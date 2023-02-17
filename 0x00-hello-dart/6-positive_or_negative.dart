ad590722b5de2ee444a9697d9f235de7ecbecee4
Tim Simms <tsimms@gmail.com>
2023-02-17 14:23:20 -0600

    task 6

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var number = int.parse(args[0]);
+  if (number > 0)
+    print("${number} is positive");
+  if (number < 0)
+    print("${number} is negative");
+  if (number == 0)
+    print("0 is zero");
+
+}
