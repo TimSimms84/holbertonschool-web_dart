62ae33892a30fd0292bf3ad71f0026f5e0b9757b
Tim Simms <tsimms@gmail.com>
2023-02-18 13:01:07 -0600

    task 10

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,21 @@
+import '9-palindrome.dart';
+
+
+String longestPalindrome(String s)
+{
+  String longest = "";
+  for (var i = 0; i < s.length; i++) {
+    for (var j = i + 1; j < s.length; j++) {
+      var substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
+      }
+    }
+  }
+  // if (longest.isEmpty) {
+  //   return "none";
+  // }
+  return longest.isEmpty ? "none" : longest;
+}
+
+
