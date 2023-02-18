7572788a2412560701fb7f48f4bd19d5f1483fd7
Tim Simms <tsimms@gmail.com>
2023-02-18 13:37:11 -0600

    init repo/task 0

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,12 @@
+class User {
+  // Properties
+  String name;
+
+  // Constructor
+  User(this.name);
+
+  // Methods
+  String showName() {
+    return 'Hello $name';
+  }
+}
---
+++ 0x02-dart-classes/README.md
@@ -0,0 +1,3 @@
+0x02. Dart - Classes
+
+[Link](https://intranet.hbtn.io/projects/2833)
