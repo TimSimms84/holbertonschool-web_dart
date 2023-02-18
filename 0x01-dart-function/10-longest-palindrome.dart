c368b6c83f6e71bb8b6af649e6a3dd7c67425792
Tim Simms <tsimms@gmail.com>
2023-02-18 13:03:15 -0600

    return the full string if it's a palidrome already

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -3,6 +3,8 @@ import '9-palindrome.dart';
 
 String longestPalindrome(String s)
 {
+  if (isPalindrome(s))
+    return s;
   String longest = "";
   for (var i = 0; i < s.length; i++) {
     for (var j = i + 1; j < s.length; j++) {
