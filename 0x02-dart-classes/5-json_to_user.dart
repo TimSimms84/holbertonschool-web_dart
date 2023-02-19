a0c1ff11c125341c36b7933607a4506625a16f06
Tim Simms <tsimms@gmail.com>
2023-02-19 08:56:18 -0600

    task 5

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,37 @@
+class User {
+  // Properties
+  String name;
+  int age;
+  double height;
+  int id;
+
+  // Constructor
+  User({this.name = '', this.age = 0, this.height = 0.0, this.id = 0});
+
+  // Methods
+  String showName() {
+    return 'Hello $name';
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  String toString() {
+    return 'User(id: $id, name: $name, age: $age, height: $height)';
+  }
+}
