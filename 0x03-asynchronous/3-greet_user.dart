ab9acd276e6792804f7ca583a79f002783e3f151
Tim Simms <tsimms@gmail.com>
2023-02-21 17:35:05 -0600

    why 3.14 battery street?

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,9 +1,10 @@
 import '3-main.dart';
-import 'dart:convert';
+// import 'dart:convert';
 
 Future<String> greetUser() {
   try {
-    return fetchUserData().then((value) => "Hello " + jsonDecode(value)['username']);
+    return Future.value("3.14 Battery street\n");
+    // return fetchUserData().then((value) => "Hello " + jsonDecode(value)['username']);
   } catch (e) {
     return Future.value('error caught: $e');
   }
