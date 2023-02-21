4d4778cf104009a42708473925ffb825babd01b4
Tim Simms <tsimms@gmail.com>
2023-02-20 20:05:59 -0600

    add main file to this file for checker

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,8 +1,19 @@
-import '2-main.dart';
+
+
 Future<void> getUser() async {
   try {
     print(await fetchUser());
   } catch (e) {
     print("error caught: $e");
   }
+}
+Future<String> fetchUser() =>
+    // Imagine that this function is
+    // more complex and slow.
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
+main() async {
+  getUser();
 }
